3c746c1f5bef859683e92690c38bc59b7d57e8ab
julianoifs <julianocosta.ifs@gmail.com>
2023-11-24 22:37:08 -0300

    implementação da Class No

--- src/com/dominio/No.java
+++ src/com/dominio/No.java
@@ -1,5 +1,29 @@
 package com.dominio;
 
 public class No<T> {
-    
+
+    private T valor;
+    private No<T> proximo;
+
+    public No(T valor) {
+        this.valor = valor;
+        this.proximo = null;
+    }
+
+    public T getValor() {
+        return valor;
+    }
+
+    public void setValor(T valor) {
+        this.valor = valor;
+    }
+
+    public No<T> getProximo() {
+        return proximo;
+    }
+
+    public void setProximo(No<T> proximo) {
+        this.proximo = proximo;
+    }
+
 }
