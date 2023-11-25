839ff0dfb77095f271eaeadcfe1e48da1256645a
julianoifs <julianocosta.ifs@gmail.com>
2023-11-25 09:16:52 -0300

    Atualizações da classe lista Encadeada

--- src/com/dominio/ListaEncadeada.java
+++ src/com/dominio/ListaEncadeada.java
@@ -1,7 +1,40 @@
 package com.dominio;
 
 public class ListaEncadeada<T> {
-    
-    
+
+    private No<T> inicio;
+    private No<T> ultimo;
+    private int tamanho;
+
+    public ListaEncadeada() {
+        this.inicio = null;
+        this.ultimo = null;
+        this.tamanho = 0;
+    }
+
+    public void addInicio(T elemento) {
+        No<T> no = new No<T>(elemento);
+
+        if (this.tamanho == 0) {
+            // this.inicio = no;
+            this.ultimo = no;
+        } else {
+            no.setProximo(this.inicio);
+        }
+        this.inicio = no;
+        this.tamanho++;
+    }
+
+    public void addFinal(T elemento) {
+        No<T> no = new No<T>(elemento);
+
+        if (this.tamanho == 0) {
+            this.inicio = no;
+        } else {
+            this.ultimo.setProximo(no);
+        }
+        this.ultimo = no;
+        this.tamanho = 0;
+    }
 
 }
--- src/com/dominio/Main.java
+++ src/com/dominio/Main.java
@@ -3,5 +3,10 @@ package com.dominio;
 public class Main {
     public static void main(String[] args) {
 
+        ListaEncadeada<Integer> lista = new ListaEncadeada<>();
+
+        lista.addInicio(1);
+        lista.addInicio(2);
+
     }
 }
